cfe92738abac4e9bb4a1ccb7328f23015f67a206
matalyn2 <matalyn.nicole@gmail.com>
2017-04-17 13:56:12 -0600

    Add files via upload

---
+++ byucomponents/header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+	<?php pgsf_wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> >
+
+	<header class="page-header">
+		<?php pgsf_the_byu_header(); ?>
+	</header>
+
+	<main id="content" class="site-content">
