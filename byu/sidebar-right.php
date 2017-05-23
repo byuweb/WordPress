50c98dd68462aa257de78dc32ddf732d51276d39
Matalyn <matalyn@PGs-Administrator.local>
2017-05-23 09:56:39 -0600

    Created branch for just parent theme

--- byu/sidebar-right.php
+++
@@ -1,5 +0,0 @@
- <?php if ( is_active_sidebar( 'sidebar-right' ) ): ?>
-  	<div class="sidebar omega">
-  		<?php dynamic_sidebar( 'sidebar-right' ) ?>
-	</div>
-<?php endif; ?>
--- byu/sidebar.php
+++
@@ -1,5 +0,0 @@
- <?php if ( is_active_sidebar( 'sidebar-left' ) ): ?>
-  	<div class="sidebar alpha">
-  		<?php echo "This is going where?" ?>dynamic_sidebar( 'sidebar-left' )
-	</div>
-<?php endif; ?>
