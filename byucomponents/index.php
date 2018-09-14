2a7619affc10f2ae58ba36be91c3c259905ea934
Blake Morgan <b.morgan.296@gmail.com>
2018-09-14 12:21:34 -0600

    Revert "Delete redundant byucomponent folder"
    This reverts commit d893d96b041ba8f6d9e565323a308986a1c77c52.

---
+++ byucomponents/index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Index Page
+*/
+?>
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			
+			<p>The content you were looking for does not exist.</p>
+
+			<a href="<?php echo esc_url( get_site_url() ); ?>">Home Page</a>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ byucomponents/templates/pgsf-home.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: Home
+*/
+?>
+
+<?php get_header(); ?>
+
+	<h1>Home Page</h1>
+			
+<?php get_footer();
